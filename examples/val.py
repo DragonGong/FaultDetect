718ea455f9521b3a7410426ab246b41bc387977a
DragonGong <dragongong20040318@gmail.com>
2025-05-24 18:16:15 +0800

    val

--- examples/val.py
+++ examples/val.py
@@ -9,11 +9,11 @@ model_state = torch.load(model_state_path, map_location=device , weights_only= F
 state_dict = model_state.state_dict()
 model.load_state_dict(state_dict)
 model.eval()
-image_path = '/Volumes/My Passport/dataset/kitti/data/train/00002264.png'
+image_path = '/Volumes/My Passport/dataset/kitti/data/test/00000128.png'
 image = Image.open(image_path).convert("RGB")
 image_tensor = model.transform_val(image).to(device)
 input_tensor = image_tensor.unsqueeze(0)
 if __name__ == '__main__':
     outputs = model(input_tensor)
     _, batch_max = outputs.max(1)
-    print(batch_max[0].item())
+    print(batch_max[0].item())  # 0 未遮挡  1 遮挡
