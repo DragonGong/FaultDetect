04a29958ad532eeeea109a413598a5d6333681d7
DragonGong <dragongong20040318@gmail.com>
2025-07-22 16:43:06 +0800

    chore:train and test new model trained with different datasets.

--- examples/vision/tasks.py
+++ examples/vision/tasks.py
@@ -31,12 +31,28 @@ def task_2():
     detect.detect_realtime_from_one_camera(q, "mps", opt=print_func, show_image=True)
 
 
+# detect by 2 cameras
 def task_3():
     save_path = "assets/image"
     camera_readers = [CameraReader(usb_port=0, save_location=save_path),
                       CameraReader(usb_port=1, save_location=save_path)]
     model = OcclusionDetectionModel()
-    service = ModelService(model, '/Volumes/My Passport/dataset/models/trained/best_resnet_for_faultdetect.pth', "mps")
+    service = ModelService(model, '/Volumes/My Passport/dataset/models/trained/7_22/best.pth', "mps")
+    detect = CameraDetect(service, camera_readers)
+    q = Queue()
+
+    def print_func(io: ModelIO):
+        print(io.odm_io)
+
+    detect.detect_realtime_from_cameras_serial(q, "mps", opt=print_func, show_image=True)
+
+
+# detect by 1 camera
+def task_4():
+    save_path = "assets/image"
+    camera_readers = [CameraReader(usb_port=0, save_location=save_path)]
+    model = OcclusionDetectionModel()
+    service = ModelService(model, '/Volumes/My Passport/dataset/models/trained/7_22/best.pth', "mps")
     detect = CameraDetect(service, camera_readers)
     q = Queue()
 
@@ -49,4 +65,4 @@ def task_3():
 if __name__ == "__main__":
     # task_1()
     # task_2()
-    task_3()
+    task_4()
--- examples/vision/train.py
+++ examples/vision/train.py
@@ -14,9 +14,9 @@ transform = transforms.Compose(
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]
 )
-dataset = OcclusionDetectionDataset(data_dir='/Volumes/My Passport/dataset/kitti/data/train',
-                                    label_file='/Volumes/My Passport/dataset/kitti/data/train/labels.txt',
-                                    transform = transform
+dataset = OcclusionDetectionDataset(data_dir='/Volumes/My Passport/dataset/kitti/data2/train',
+                                    label_file='/Volumes/My Passport/dataset/kitti/data2/train/labels.txt',
+                                    transform=transform
                                     )
 train_size = int(0.7 * len(dataset))
 val_size = len(dataset) - train_size
@@ -25,10 +25,11 @@ train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
-model = OcclusionDetectionModel(load_pretrained=True,base_model_path='/Volumes/My Passport/dataset/models/pretrained/resnet18-f37072fd.pth'
+model = OcclusionDetectionModel(load_pretrained=True,
+                                base_model_path='/Volumes/My Passport/dataset/models/pretrained/resnet18-f37072fd.pth'
                                 , freeze_pretrained=True)
 loss = nn.CrossEntropyLoss()
-opt = Adam(filter(lambda p:p.requires_grad ,  model.parameters()), lr=0.001)
+opt = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device('mps')
@@ -42,7 +43,7 @@ def train_model(model, train_loader, val_loader, loss_func, opt, device=torch.de
 
     for epoch in tqdm(range(nums_epoch)):
         train_loss = 0.0
-        with tqdm(train_loader,desc='train') as t:
+        with tqdm(train_loader, desc='train') as t:
             for images, labels in t:
                 images, labels = images.to(device), labels.to(device)
                 opt.zero_grad()
@@ -58,7 +59,7 @@ def train_model(model, train_loader, val_loader, loss_func, opt, device=torch.de
 
         val_loss = 0.0
         correct = 0
-        with tqdm(val_loader,desc='val') as t:
+        with tqdm(val_loader, desc='val') as t:
             for images, labels in t:
                 images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
@@ -66,13 +67,13 @@ def train_model(model, train_loader, val_loader, loss_func, opt, device=torch.de
                 val_loss += running_loss.item() * images.size(0)
                 _, pred = outputs.max(1)
                 correct += pred.eq(labels).sum(0).item()
-                t.set_postfix(loss = running_loss.item())
+                t.set_postfix(loss=running_loss.item())
         val_loss = val_loss / len(val_loader.dataset)
         val_acc = 100. * correct / len(val_loader.dataset)
         if val_loss < best_val_loss:
             best_model = model.state_dict().copy()
             best_val_loss = val_loss
-            torch.save(best_model,'/Volumes/My Passport/dataset/models/trained/best_resnet_for_faultdetect.pth')
+            torch.save(best_model, '/Volumes/My Passport/dataset/models/trained/7_22/best.pth')
             print(f'Saved best model at epoch {epoch + 1}')
         print(f'Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.2f}%')
     model.load_state_dict(best_model)
@@ -80,6 +81,5 @@ def train_model(model, train_loader, val_loader, loss_func, opt, device=torch.de
 
 
 if __name__ == '__main__':
-    model = train_model(model, train_loader, val_loader, loss, opt, device, 100)
-    torch.save(model.state_dict(),'/Volumes/My Passport/dataset/models/trained/final_resnet_for_faultdetect.pth')
-
+    model = train_model(model, train_loader, val_loader, loss, opt, device, 30)
+    torch.save(model.state_dict(), '/Volumes/My Passport/dataset/models/trained/7_22/final.pth')
